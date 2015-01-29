7c15f6ecff1c5e7e783fd07ab63c73ca652f0b82
Tri Huynh <thuynh68@hotmail.com>
2015-01-29 21:50:46 +0000

    Fixed indentation problems in put function

--- snippets.py
+++ snippets.py
@@ -7,10 +7,10 @@ logging.basicConfig(filename="output.log", level=logging.DEBUG)
 def put(name, snippet, filename):
   """ Store a snippet with an associated name in the CSV file """
   logging.info("Writing {!r}:{!r} to {!r}".format(name, snippet, filename))
-    logging.debug("Opening file")
-    with open(filename, "a") as f:
-        writer = csv.writer(f)
-        logging.debug("Writing snippet to file")
-        writer.writerow([name, snippet])
-    logging.debug("Write sucessful")
-    return name, snippet
+  logging.debug("Opening file")
+  with open(filename, "a") as f:
+      writer = csv.writer(f)
+      logging.debug("Writing snippet to file")
+      writer.writerow([name, snippet])
+  logging.debug("Write sucessful")
+  return name, snippet
